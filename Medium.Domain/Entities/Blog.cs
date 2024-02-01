406c94bc3328e5be4fab3d14441a65ab3f4c6343
Asif <asifabdullah135@gmail.com>
2024-02-01 21:19:54 +0600

    add entities

---
+++ Medium.Domain/Entities/Blog.cs
@@ -0,0 +1,10 @@
+﻿namespace Medium.Domain.Entities;
+
+public class Blog
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = "";
+    public string Content { get; set; } = "";
+    public Guid UserId { get; set; }
+    public User? User { get; set; }
+}
---
+++ Medium.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Medium.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public required string UserName { get; set; }
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string Address { get; set; } = "";
+    public List<Blog>? Blogs { get; set; }
+
+}
