cce2d050d079d51303d135878aaf64ecfdb90534
Asif <asifabdullah135@gmail.com>
2024-02-01 21:20:12 +0600

    add DB Schema

---
+++ Medium.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Medium.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Medium.Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Blog> Blogs => Set<Blog>();
+}
