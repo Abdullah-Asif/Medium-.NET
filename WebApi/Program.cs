4c0027bbcbca78571e6a9395fab565fda10bac07
Asif <asifabdullah135@gmail.com>
2024-02-01 21:20:47 +0600

    configure DB Schema

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,19 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Net;
+using Medium.Application.Interfaces;
+using Medium.Infrastructure;
+using Medium.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Endpoints;
 
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"), 
+        b => b.MigrationsAssembly(typeof(Program).Assembly.FullName)));
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IUserService, UserService>();
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
+app.RegisterUserEndpoints();
 
-app.MapGet("/", () =>
-{
-    return "Hello world";
-});
 app.Run();
