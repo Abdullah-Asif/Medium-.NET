ace0c3fa33d8a274642c245de82f739777b7cbf5
Asif <asifabdullah135@gmail.com>
2024-02-19 11:09:52 +0600

    interfaces added

---
+++ Medium.Application/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using Medium.Application.Utilities;
+using Medium.Domain.Entities;
+
+namespace Medium.Application.Interfaces;
+
+public interface IRepository<TEntity>
+{
+    Task<List<TEntity>> GetAllAsync(PaginationQueryRequest paginationQueryRequest,bool tracked = true);
+    Task<List<TEntity>> GetAllFilteredAsync(Expression<Func<TEntity, bool>> filter, PaginationQueryRequest paginationQueryRequest, bool tracked = true);
+    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>>? filter, bool tracked = true);
+    Task<TEntity> GetByIdAsync(string id);
+    Task CreateAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task RemoveAsync(TEntity entity);
+    IQueryable<TEntity> GetQueryableObject();
+    Task SaveAsync();
+}
---
+++ Medium.Application/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Medium.Application.DTOs;
+using Medium.Application.Utilities;
+using Medium.Domain.Entities;
+
+namespace Medium.Application.Interfaces;
+
+public interface IUserService
+{
+    Task<List<UserDto>> GetAllUsers(PaginationQueryRequest paginationQueryRequest);
+    Task CreateUser(SignUpDto signUpDto);
+    Task<UserDto> GetUserById(string id);
+    Task<UserDto> GetUserByName(string name);
+    Task<UserDto> GetUserByEmail(string email);
+    Task UpdateUser(string id, UserDto user);
+    Task DeleteUser(string id);
+}
