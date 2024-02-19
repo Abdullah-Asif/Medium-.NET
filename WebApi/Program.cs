1a4483e7876730bb891bd903aece0959b60001b3
Asif <asifabdullah135@gmail.com>
2024-02-19 11:13:23 +0600

    register endpoints and seeding code added

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,18 +1,13 @@
-using System.Net;
-using Medium.Application.Interfaces;
-using Medium.Infrastructure;
-using Medium.Infrastructure.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using WebApi.Endpoints;
+using Bogus;
+using Medium.Application;
+using Medium.Domain.Entities;
+using WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"), 
-        b => b.MigrationsAssembly(typeof(Program).Assembly.FullName)));
-builder.Services.AddSwaggerGen();
-builder.Services.AddTransient<IUserService, UserService>();
+
+builder.Services.RegisterWebApiServices(builder.Configuration);
+builder.Services.RegisterApplicationServices();
+builder.Services.RegisterInfrastructureServices();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -22,5 +17,50 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 app.RegisterUserEndpoints();
+app.RegisterBlogEndpoints();
+app.RegisterAuthEndpoint();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapPost("fill", async (ApplicationDbContext dbContext) =>
+{
+    using var transaction = await dbContext.Database.BeginTransactionAsync();
+    try
+    {
+        await dbContext.Users.ExecuteDeleteAsync();
+        await dbContext.Blogs.ExecuteDeleteAsync();
+
+        var userFaker = new Faker<User>()
+            .RuleFor(u => u.Id, _ => new Guid())
+            .RuleFor(u => u.UserName, f => f.Person.UserName)
+            .RuleFor(u => u.FirstName, f => f.Person.FirstName)
+            .RuleFor(u => u.LastName, f => f.Person.LastName)
+            .RuleFor(u => u.Email, f => f.Person.Email)
+            .RuleFor(u => u.Password, f => f.Internet.Password())
+            .RuleFor(u => u.Address, f => f.Address.FullAddress());
+        
+        var users = userFaker.Generate(100);
+
+        var blogFaker = new Faker<Blog>().
+            RuleFor(b => b.Id, _ => new Guid())
+            .RuleFor(b => b.Title, f => f.Lorem.Sentence())
+            .RuleFor(b => b.Content, f => f.Lorem.Sentences())
+            .RuleFor(b => b.User, f => f.PickRandom(users));
+
+        var blogs = blogFaker.Generate(100);
+        await dbContext.Users.AddRangeAsync(users);
+        await dbContext.Blogs.AddRangeAsync(blogs);
+
+        await dbContext.SaveChangesAsync();
+
+        await dbContext.Database.CommitTransactionAsync();
+    }
+    catch
+    {
+        await dbContext.Database.RollbackTransactionAsync();
+        throw;
+    }
+});
 
 app.Run();
