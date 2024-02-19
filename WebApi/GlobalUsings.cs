d28ad568f3f622add3b1a33367e8055e55b6f323
Asif <asifabdullah135@gmail.com>
2024-02-19 11:12:50 +0600

    added using

---
+++ WebApi/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using System.Net;
+global using Medium.Application.Interfaces;
+global using Medium.Application.Services;
+global using Medium.Infrastructure;
+global using Medium.Infrastructure.Services;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using WebApi.Endpoints;
