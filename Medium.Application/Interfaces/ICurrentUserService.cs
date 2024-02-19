19fe1f834e739e0fbe568999238dd47ccdaf7d6f
Asif <asifabdullah135@gmail.com>
2024-02-19 11:10:37 +0600

    current user info fetching

---
+++ Medium.Application/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,7 @@
+﻿namespace Medium.Application.Interfaces;
+
+public interface ICurrentUserService
+{
+    public string UserId { get; }
+    public string Email { get; }
+}
---
+++ WebApi/Services/CurrentUserService.cs
@@ -0,0 +1,16 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace WebApi.Services;
+
+public class CurrentUserService : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+
+    public CurrentUserService(IHttpContextAccessor contextAccessor)
+    {
+        _contextAccessor = contextAccessor;
+    }
+    public string UserId => _contextAccessor.HttpContext.User.FindFirstValue("Id");
+    public string Email => _contextAccessor.HttpContext.User.FindFirstValue(JwtRegisteredClaimNames.Email);
+}
